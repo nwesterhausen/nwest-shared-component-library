ba4186997f8c8b4ad1ebfa20c8168cbbd9d7c132
Nick Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-07-17 16:58:42 -0400

    feat: add `DecimalAttribute`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 //! Hello
 //!
 
+pub mod decimal_attribute;
 pub mod errors;
 pub mod integer_attribute;
 
+pub use decimal_attribute::DecimalAttribute;
 pub use errors::AttributeError;
 pub use integer_attribute::IntegerAttribute;
