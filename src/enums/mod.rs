2ebe584167bfe1dc6374d847921fe6c594defd1f
Nick Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-07-18 08:49:23 -0400

    feat: lift skill enum from game_library

--- src/enums/mod.rs
+++ src/enums/mod.rs
@@ -1,11 +1,13 @@
 //! Module with the enums and their implementations.
 
 pub mod base_stat_enum;
+pub mod skill_enum;
 pub mod stat_enum;
 pub mod stat_modifier_enum;
 pub mod type_category_enum;
 
 pub use base_stat_enum::BaseStat;
+pub use skill_enum::Skill;
 pub use stat_enum::Stat;
 pub use stat_modifier_enum::StatModifier;
 pub use type_category_enum::TypeCategory;
