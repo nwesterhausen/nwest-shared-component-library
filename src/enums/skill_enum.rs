0268988f4a04a78330b35c422d6ae0890ec3370d
Nick Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-08-12 09:38:35 -0400

    feat: add some more skills

--- src/enums/skill_enum.rs
+++ src/enums/skill_enum.rs
@@ -8,8 +8,9 @@ use serde::{Deserialize, Serialize};
 /// Skills which could be used to level up a character. These are intended to then be used
 /// to increase the character's abilities, or to unlock new abilities/spells/attacks.
 ///
-/// These are derived from a latin or greek root word, followed by the suffix "-mancy" for
-/// the school of magic that the skill belongs to. Other skills are not necessarily magic-based.
+/// These are derived from a latin or greek root word.
+///
+/// Magic skills are suffixed with "mancy" to indicate that they are a form of magic.
 #[derive(Serialize, Deserialize, Clone, Copy, Component, Resource, PartialEq, Eq, Hash)]
 pub enum Skill {
     /// Pyromancy is the school of fire magic.
@@ -48,4 +49,18 @@ pub enum Skill {
     Mutatiomancy,
     /// Chronomancy is the school of time magic.
     Chronomancy,
+    /// Spatiomancy is the school of space magic.
+    Spatiomancy,
+    /// Gravitamancy is the school of gravity magic.
+    Gravitamancy,
+    /// Phantasmamancy is the school of illusion magic.
+    Phantasmamancy,
+    /// Malamancy is the school of curse magic.
+    Malamancy,
+    /// Beneficamancy is the school of blessing magic.
+    Beneficamancy,
+    /// Cognimancy is the school of mental magic.
+    Cognimancy,
+    /// Medicamancy is the school of healing magic.
+    Medicamancy,
 }
